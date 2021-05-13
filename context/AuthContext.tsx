6d08dc486623fa7111666c36b12b14e67f3351e8
Augusto Pohl <augustopohl@hotmail.com>
2021-05-13 17:16:50 -0300

    authentication context, axios client, saving tokens in cookies

---
+++ context/AuthContext.tsx
@@ -0,0 +1,69 @@
+import { createContext, ReactNode, useState } from "react";
+import { setCookie } from 'nookies'
+import Router from 'next/router';
+import { api } from "../services/api";
+
+type SignInCredentials = {
+    email: string;
+    password: string;
+}
+
+type AuthContextData = {
+    signIn(credentials: SignInCredentials): Promise<void>;
+    user: User;
+    isAuthenticated: boolean;
+}
+
+type AuthProviderProps = {
+    children: ReactNode;
+}
+
+type User = {
+    email: string;
+    permissions: string[];
+    roles: string[];
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+    const [ user, setUser ] = useState<User>()
+
+    const isAuthenticated = !!user;
+
+    async function signIn({ email, password }: SignInCredentials) { // must be asyncronous to validate the Promise
+        try {
+            const response = await api.post('sessions', {
+                email,
+                password,
+            })
+
+            const { token, refreshToken, permissions, roles } = response.data;
+
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            })
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            })
+
+            setUser({
+                email,
+                permissions,
+                roles,
+            })
+
+            Router.push('/dashboard')
+         } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ signIn, isAuthenticated, user}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
